4dcdd057354e2047000abda3c444675c2d2bae17
GabrielaFabíola <gaabstudies@gmail.com>
2022-03-07 23:47:54 -0300

    Add

---
+++ desafio-programacao-capgemini-02-main/desafio-programacao-capgemini-02-main/README.md
@@ -0,0 +1,25 @@
+
+# Desafio de Programação 02
+## Tecnologias Utilizadas: 
+
+ Eclipse 4.22 \
+ Linguagem de Programação: Java. \
+ Jdk Version: 11.0.6
+
+## Instruções para rodar os exercícios: 
+**Exercício 01:** 
+A mediana de uma lista de números é basicamente o elemento que se encontra no meio da lista após a ordenação. Dada uma lista de números com um número ímpar de elementos, desenvolva um algoritmo que encontre a mediana
+
+**Exercício 02:** 
+    Dado um vetor de inteiros n e um inteiro qualquer x. Construa um algoritmo que determine o número de elementos pares do vetor que tem uma diferença igual ao valor de x. 
+
+
+**Exercício 03:** 
+Um texto precisa ser encriptado usando o seguinte esquema. Primeiro, os espaços são removidos do texto. Então, os caracteres são escritos em um grid, no qual as linhas e colunas tem as seguintes regras:
+
+  <=linha<=coluna<=
+
+Considere T, como o tamanho do texto.
+Se certifique de que linhas x colunas .
+Se múltiplos grids satisfazem as condições, escolha aquele com a menor área.
+Escreva um algoritmo que ao receber uma string s, mostre a mensagem encriptada de acordo com as regras descritas.
---
+++ desafio-programacao-capgemini-02-main/desafio-programacao-capgemini-02-main/src/Questao02.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Questao02 {
+    public static void main(String[] args) {
+    	
+        // Cria um objeto "sc" do tipo Scanner para entrada de dados.
+        Scanner sc = new Scanner(System.in);
+
+        int[] vetor = new int[5]; // Criaçã do vetor.
+
+        // Preenche o vetor com os valores de entrada
+        for(int i = 0; i < 5; i++){
+            System.out.print("Digite um valor para a posição " + i + " do vetor: ");
+            vetor[i] = sc.nextInt();
+        }
+
+        int cont = 0; // Contador que irá armazenar o número de elementos pares do vetor que tem uma diferença igual ao valor de x.
+        System.out.print("Digite um valor para x: ");
+        int x = sc.nextInt(); // Recebe um valor para x.
+
+        // Cada valor do vetor será comparado com todos os valores a sua frente no intuito de encontrar os pares de valores cujo a diferença é igual a "x".
+        for(int i = 0; i < vetor.length; i++){
+            for(int j = i + 1; j < vetor.length; j++) {
+                if(Math.abs(vetor[i] - vetor[j]) == x){ // Utilizei o Math.abs para retornar o valor absoluto da subtração.
+                    cont++; // O contador será encrementado cada vez que a condição do if for atendida.
+                }
+            }
+        }
+        System.out.println("\nResultado: " + cont); // Imprime o valor do contador.
+    }
+}
